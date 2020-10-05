4309aff22dd2ca2ca020f324be548880b6b3a1de
Mitchell Robert <github@mrincworld.com>
2020-10-05 22:21:29 +0200

    Updated installer

--- installer.js
+++ installer.js
@@ -1,12 +1,12 @@
 const OS = require( 'os' );
 
-exports.default = ( jsonConfig ) => {
+exports.default = ( jsonConfig, pluginName ) => {
   const identity = OS.hostname() || Math.random().toString( 36 ).substring( 2, 15 ) + Math.random().toString( 36 ).substring( 2, 15 );
   jsonConfig.identity = jsonConfig.identity || identity;
   jsonConfig.plugins = jsonConfig.plugins || {};
-  jsonConfig.plugins.ws = jsonConfig.plugins.ws || {};
-  jsonConfig.plugins.ws.endpoint = jsonConfig.plugins.ws.endpoint || "wss://localhost";
-  jsonConfig.plugins.ws.identity = jsonConfig.plugins.ws.identity || identity;
+  jsonConfig.plugins[pluginName] = jsonConfig.plugins[pluginName] || {};
+  jsonConfig.plugins[pluginName].endpoint = jsonConfig.plugins[pluginName].endpoint || "ws://localhost";
+  jsonConfig.plugins[pluginName].identity = jsonConfig.plugins[pluginName].identity || identity;
 
   return jsonConfig;
 }
