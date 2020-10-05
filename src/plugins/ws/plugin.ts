4afb8afdb5cbfe9104bc6dcfe517a1e856262681
Mitchell Robert <github@mrincworld.com>
2020-10-05 22:09:49 +0200

    Updated base lib and class conformity

--- src/plugins/ws/plugin.ts
+++ src/plugins/ws/plugin.ts
@@ -1,85 +1,95 @@
 import * as WEBSOCKETS from 'ws';
 import * as os from 'os';
-import { IEmitter, PluginFeature } from '@bettercorp/service-base/lib/ILib';
+import { IPlugin, PluginFeature } from '@bettercorp/service-base/lib/ILib';
 import { Tools as TOOLS } from '@bettercorp/tools/lib/Tools';
 import { WSEvent } from './WSEvent';
 
-let WebSocket: WEBSOCKETS;
-let reconnect = (features: PluginFeature) => {
-  features.log.info(`[WS] Connect to[${features.config.plugins.ws.endpoint}]`);
-  WebSocket = new WEBSOCKETS(features.config.plugins.ws.endpoint, {
-    perMessageDeflate: false
-  });
-  WebSocket.on('error', function () {
-    features.log.info('[WS] Errored out');
-    features.emitEvent(`ws-status`, false);
-  });
-  WebSocket.on('close', function () {
-    features.log.info('[WS] Disconnected');
-    features.emitEvent(`ws-status`, false);
-    WebSocket.close();
-    WebSocket.terminate();
-    setTimeout(() => {
-      reconnect(features);
-    }, 5000);
-  });
-  WebSocket.on('open', function open () {
-    features.log.info('[WS] Connected');
-    WebSocket.send(JSON.stringify({
-      action: 'ws-auth',
-      auth: features.config.plugins.ws.token || '',
-      data: {
-        name: features.config.identity || features.config.plugins.ws.identity || os.hostname
-      }
-    }));
-    setTimeout(() => {
-      features.emitEvent(`ws-status`, true);
-    }, 5000);
-  });
+export class Plugin implements IPlugin {
+  private WebSocket!: WEBSOCKETS;
+  private Features!: PluginFeature;
 
-  WebSocket.on('message', function incoming (data: string) {
-    let msg: WSEvent = JSON.parse(data);
-    if (msg.action === 'log') {
-      return features.log.info(`[SERVER-${msg.action}] ${msg.data}`);
-    }
-    features.log.info(data);
-    if (TOOLS.isNullOrUndefined(msg.action)) return;
-    if (TOOLS.isNullOrUndefined(msg.data)) return;
-    features.emitEvent('message', false, msg);
-  });
-};
-module.exports.init = (features: PluginFeature) => {
-  if (TOOLS.isNullOrUndefined(features.config.plugins)) {
-    throw `No plugins definition in config file!`;
-  }
-  if (TOOLS.isNullOrUndefined(features.config.plugins.ws)) {
-    throw `No ws plugin definition in config file!`;
-  }
-  if (TOOLS.isNullOrUndefined(features.config.plugins.ws.endpoint)) {
-    throw `No ws plugin endpoint definition in config file!`;
-  }
-  if (TOOLS.isNullOrUndefined(features.config.plugins.ws.token)) {
-    throw `No ws plugin token definition in config file!`;
-  }
-
-  reconnect(features);
-
-  features.onEvent('send', false, (data: IEmitter<WSEvent>) => {
-    if (TOOLS.isNullOrUndefined(data.data)) return;
-    if (TOOLS.isNullOrUndefined(data.data.action)) return;
-    if (typeof data.data.action !== 'string') return;
-
-    if (WebSocket === undefined || WebSocket === null) {
-      return features.log.warn('WS NOT CONNECTED YET');
-    }
-    try {
-      WebSocket.send(JSON.stringify({
-        action: data.data.action,
-        data: data.data.data,
-        auth: features.config.plugins.ws.token || '',
+  private reconnect () {
+    const self = this;
+    self.Features.log.info(`[WS] Connect to[${self.Features.config.plugins.ws.endpoint}]`);
+    self.WebSocket = new WEBSOCKETS(self.Features.config.plugins.ws.endpoint, {
+      perMessageDeflate: false
+    });
+    self.WebSocket.on('error', function () {
+      self.Features.log.info('[WS] Errored out');
+      self.Features.emitEvent(null, `status`, false);
+    });
+    self.WebSocket.on('close', function () {
+      self.Features.log.info('[WS] Disconnected');
+      self.Features.emitEvent(null, `status`, false);
+      self.WebSocket.close();
+      self.WebSocket.terminate();
+      setTimeout(() => {
+        self.reconnect();
+      }, 5000);
+    });
+    self.WebSocket.on('open', function open () {
+      self.Features.log.info('[WS] Connected');
+      self.WebSocket.send(JSON.stringify({
+        action: 'ws-auth',
+        auth: self.Features.config.plugins.ws.token || '',
+        data: {
+          name: self.Features.config.identity || self.Features.config.plugins.ws.identity || os.hostname
+        }
       }));
-    } catch (exc) {
-      features.log.error(exc);
-    }
-  });
+      setTimeout(() => {
+        self.Features.emitEvent(null, `status`, true);
+      }, 5000);
+    });
+
+    self.WebSocket.on('message', function incoming (data: string) {
+      let msg: WSEvent = JSON.parse(data);
+      if (msg.action === 'log') {
+        return self.Features.log.info(`[SERVER-${msg.action}] ${msg.data}`);
+      }
+      self.Features.log.info(data);
+      if (TOOLS.isNullOrUndefined(msg.action)) return;
+      if (TOOLS.isNullOrUndefined(msg.data)) return;
+      self.Features.emitEvent(null, 'message', msg);
+    });
+  }
+
+  init (features: PluginFeature): Promise<void> {
+    this.Features = features;
+    const self = this;
+    return new Promise((resolve) => {
+      if (TOOLS.isNullOrUndefined(features.config.plugins)) {
+        throw `No plugins definition in config file!`;
+      }
+      if (TOOLS.isNullOrUndefined(features.config.plugins.ws)) {
+        throw `No ws plugin definition in config file!`;
+      }
+      if (TOOLS.isNullOrUndefined(features.config.plugins.ws.endpoint)) {
+        throw `No ws plugin endpoint definition in config file!`;
+      }
+      if (TOOLS.isNullOrUndefined(features.config.plugins.ws.token)) {
+        throw `No ws plugin token definition in config file!`;
+      }
+
+      self.reconnect();
+
+      features.onEvent<WSEvent>(null, 'send', (data) => {
+        if (TOOLS.isNullOrUndefined(data.data)) return;
+        if (TOOLS.isNullOrUndefined(data.data.action)) return;
+        if (typeof data.data.action !== 'string') return;
+
+        if (WebSocket === undefined || WebSocket === null) {
+          return features.log.warn('WS NOT CONNECTED YET');
+        }
+        try {
+          self.WebSocket.send(JSON.stringify({
+            action: data.data.action,
+            data: data.data.data,
+            auth: features.config.plugins.ws.token || '',
+          }));
+        } catch (exc) {
+          features.log.error(exc);
+        }
+      });
+    });
+  }
 };
