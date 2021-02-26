f54f41c37e534449f7dbb90db76ea4329e981aec
Mitchell R <support@bettercorp.co.za>
2021-02-26 22:17:38 +0200

    Updated installer.js

--- installer.js
+++ installer.js
@@ -1,12 +1,5 @@
-const OS = require( 'os' );
-
 exports.default = ( jsonConfig, pluginName ) => {
-  const identity = OS.hostname() || Math.random().toString( 36 ).substring( 2, 15 ) + Math.random().toString( 36 ).substring( 2, 15 );
-  jsonConfig.identity = jsonConfig.identity || identity;
-  jsonConfig.plugins = jsonConfig.plugins || {};
-  jsonConfig.plugins[pluginName] = jsonConfig.plugins[pluginName] || {};
-  jsonConfig.plugins[pluginName].endpoint = jsonConfig.plugins[pluginName].endpoint || "ws://localhost";
-  jsonConfig.plugins[pluginName].identity = jsonConfig.plugins[pluginName].identity || identity;
-
-  return jsonConfig;
+  return {
+    port: 8082
+  }
 }
